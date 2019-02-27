c9df7d6d919704406761f4383b91a57182a69024
Smelky <46086711+Smelky@users.noreply.github.com>
2019-02-27 22:06:27 +0200

    AddSecondBranch

--- src/com/smelk/Main.java
+++ src/com/smelk/Main.java
@@ -4,7 +4,6 @@ public class Main {
 
     public static void main(String[] args) {
 	// i`m doing something here
-        // It`s First Branch
-        // And it`s something new in First Branch
+        // It`s Second Branch
     }
 }
