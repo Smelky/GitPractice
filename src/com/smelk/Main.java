a925e294e8ccf3a14d57ee0ef94fd782cddca8ce
Smelky <46086711+Smelky@users.noreply.github.com>
2019-02-27 22:21:17 +0200

    Something New In First Branch

--- src/com/smelk/Main.java
+++ src/com/smelk/Main.java
@@ -4,7 +4,7 @@ public class Main {
 
     public static void main(String[] args) {
 	// i`m doing something here
-        // It`s Second Branch
-        // And it`s something new in Second Branch
+        // It`s First Branch
+        // And it`s something new in First Branch
     }
 }
